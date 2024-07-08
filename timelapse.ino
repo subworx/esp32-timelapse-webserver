ab80a9551f49be04c05fed90ae31cef4d0cfcb4e
Sub <subworx@users.noreply.github.com>
2024-07-08 21:19:09 +0200

    initial upload
    from https://www.instructables.com/ESP32-CAM-Take-Photo-and-Save-to-MicroSD-Card-With/

---
+++ timelapse.ino
@@ -0,0 +1,249 @@
+// https://www.instructables.com/ESP32-CAM-Take-Photo-and-Save-to-MicroSD-Card-With/
+
+#include "esp_camera.h"
+#include "FS.h"                // SD Card ESP32
+#include "SD_MMC.h"            // SD Card ESP32
+#include "soc/soc.h"           // Disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "driver/rtc_io.h"
+#include <WiFi.h>
+#include "time.h"
+
+
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "xxxxxxx";
+const char* password = "xxxxxxx";
+
+
+// REPLACE WITH YOUR TIMEZONE STRING: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+String myTimezone ="CET-1CEST,M3.5.0,M10.5.0/3";
+
+
+// Pin definition for CAMERA_MODEL_AI_THINKER
+// Change pin definition if you're using another ESP32 camera module
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+
+// Stores the camera configuration parameters
+camera_config_t config;
+
+
+// Initializes the camera
+void configInitCamera(){
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  //config.xclk_freq_hz = 20000000;
+  config.xclk_freq_hz = 16000000;
+  config.pixel_format = PIXFORMAT_JPEG; //YUV422,GRAYSCALE,RGB565,JPEG
+  config.grab_mode = CAMERA_GRAB_LATEST;
+
+
+  // Select lower framesize if the camera doesn't support PSRAM
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10; //0-63 lower number means higher quality
+    config.fb_count = 1;
+    //Serial.println("psram found");
+  } else {
+    //config.frame_size = FRAMESIZE_SVGA;
+    config.frame_size = FRAMESIZE_UXGA;
+    //config.jpeg_quality = 12;
+    config.jpeg_quality = 10;
+    config.fb_count = 1;
+    //Serial.println("NO psram found");
+  }
+  
+  // Initialize the Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_brightness(s, 0);     // -2 to 2
+  s->set_contrast(s, 0);       // -2 to 2
+  s->set_saturation(s, 0);     // -2 to 2
+  s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+  s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+  s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+  s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+  s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+  s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+  s->set_ae_level(s, 0);       // -2 to 2
+  s->set_aec_value(s, 300);    // 0 to 1200
+  s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+  s->set_agc_gain(s, 0);       // 0 to 30
+  s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+  s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+  s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+  s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+  s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+  s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+  s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+  s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+  s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
+
+}
+
+
+// Connect to wifi
+void  initWiFi(){
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+}
+
+
+// Function to set timezone
+void setTimezone(String timezone){
+  Serial.printf("  Setting Timezone to %s\n",timezone.c_str());
+  setenv("TZ",timezone.c_str(),1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
+  tzset();
+}
+
+
+// Connect to NTP server and adjust timezone
+void initTime(String timezone){
+  struct tm timeinfo;
+  Serial.println("Setting up time");
+  configTime(0, 0, "pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
+  if(!getLocalTime(&timeinfo)){
+    Serial.println(" Failed to obtain time");
+    return;
+  }
+  Serial.println("Got the time from NTP");
+  // Now we can set the real timezone
+  setTimezone(timezone);
+}
+
+
+// Get the picture filename based on the current ime
+String getPictureFilename(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return "";
+  }
+  char timeString[20];
+  strftime(timeString, sizeof(timeString), "%Y-%m-%d_%H-%M-%S", &timeinfo);
+  Serial.println(timeString);
+  String filename = "/picture_" + String(timeString) +".jpg";
+  return filename; 
+}
+
+
+// Initialize the micro SD card
+void initMicroSDCard(){
+  // Start Micro SD card
+  Serial.println("Starting SD Card");
+  if(!SD_MMC.begin()){
+    Serial.println("SD Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD_MMC.cardType();
+  if(cardType == CARD_NONE){
+    Serial.println("No SD Card attached");
+    return;
+  }
+}
+
+
+// Take photo and save to microSD card
+void takeSavePhoto(){
+  // Take Picture with Camera
+  camera_fb_t * fb = esp_camera_fb_get();
+ 
+  //Uncomment the following lines if you're getting old pictures
+  //esp_camera_fb_return(fb); // dispose the buffered image
+  //fb = NULL; // reset to capture errors
+  //fb = esp_camera_fb_get();
+  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }
+
+
+  // Path where new picture will be saved in SD Card
+  String path = getPictureFilename();
+  Serial.printf("Picture file name: %s\n", path.c_str());
+  
+  // Save picture to microSD card
+  fs::FS &fs = SD_MMC; 
+  File file = fs.open(path.c_str(),FILE_WRITE);
+  if(!file){
+    Serial.printf("Failed to open file in writing mode");
+  } 
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.printf("Saved: %s\n", path.c_str());
+  }
+  file.close();
+  esp_camera_fb_return(fb); 
+}
+
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
+
+  Serial.begin(115200);
+  delay(2000);
+
+  // Initialize Wi-Fi
+  initWiFi();
+  // Initialize time with timezone
+  initTime(myTimezone);    
+  // Initialize the camera  
+  Serial.print("Initializing the camera module...");
+  configInitCamera();
+  Serial.println("Ok!");
+  // Initialize MicroSD
+  Serial.print("Initializing the MicroSD card module... ");
+  initMicroSDCard();
+}
+
+
+void loop() {    
+  // Take and Save Photo
+  takeSavePhoto();
+  // wait 3 minutes
+  delay(180000);
+  // delay 1h
+  //delay(3600000);
+}
